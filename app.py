d5b8eb169f46e3c4340308aaae4490c57030bfdf
Mehran Shahidi <m.mehrashahidi@gmail.com>
2020-01-28 16:41:43 +0330

    adding signup page

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField, RadioField, SelectField
+from wtforms.validators import InputRequired
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisisasecret!'
 
-@app.route('/<name>')
-def index(name):
-    return render_template('index.html',name=name)
+
+class MyForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+    form = MyForm()
+    if form.validate_on_submit():
+        redirect(url_for('index'))
+
+    return render_template('signup.html', form=form)
