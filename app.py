99ad75481b67a212235ee0aa789b6a1aefa2b457
Mehran Shahidi <m.mehrashahidi@gmail.com>
2020-01-31 17:44:53 +0330

    adding error report by flash

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, g
+from flask import Flask, render_template, request, redirect, url_for, g, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, RadioField, SelectField
 from wtforms.validators import InputRequired
@@ -40,11 +40,16 @@ def index():
 def signup():
     form = SignUpForm()
     if form.validate_on_submit():
-        conn, cur = get_db()
-        cur.execute('insert into users(username,email,password) values(%s,%s,%s)',
-                    (form.username.data, form.email.data, form.password.data))
-        conn.commit()
-        return redirect(url_for('index'))
+        try:
+            db = get_db()
+            conn = db[0]
+            cur = db[1]
+            cur.execute('insert into users(username,email,password) values(%s,%s,%s)',
+                        (form.username.data, form.email.data, form.password.data))
+            conn.commit()
+            return redirect(url_for('index'))
+        except:
+            flash("Couldn't conncect to database")
 
     return render_template('signup.html', form=form)
 
