5bef5553a3bc4e9acd3dc143f961bca103f6032f
Mehran Shahidi <m.mehrashahidi@gmail.com>
2020-02-02 22:48:28 +0330

    view of add_article has added

--- app/author/views.py
+++ app/author/views.py
@@ -1,5 +1,10 @@
-from flask import render_template, g, flash, url_for, redirect, session
+from flask import render_template, g, flash, url_for, redirect, session,request
 from . import author
 from .. import get_db
 from .form import AuthorFomr
 
+@author.route('/add_article',methods=['GET','POST'])
+def add_article():
+    if request.method=='POST' and request.form.get('title') and request.form.get('article'):
+        return request.form.get('article')
+    return render_template('author/add_article.html')
